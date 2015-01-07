61d10bf2e637e306893985d82c73d41b0e2e2e22
Peter Hellberg <peter@c7.se>
2015-01-08 00:16:25 +0100

    Added documentation of the command line client

--- cmd/giphy/giphy.go
+++ cmd/giphy/giphy.go
@@ -1,3 +1,41 @@
+/*
+
+A command line client for the Giphy API
+
+Installation
+
+Just go get the command:
+
+		go get -u github.com/peterhellberg/giphy/cmd/giphy
+
+Configuration
+
+The command line client can be used straight out of the box, but
+there are also a few environment variables that you can use in order
+to override the default configuration.
+
+    Environment variable  | Default value
+		----------------------|--------------
+		GIPHY_API_KEY         | dc6zaTOxFJmzC
+		GIPHY_RATING          | g
+		GIPHY_LIMIT           | 10
+		GIPHY_BASE_URL_SCHEME | http
+		GIPHY_BASE_URL_HOST   | api.giphy.com
+		GIPHY_BASE_PATH       | /v1
+		GIPHY_USER_AGENT      | giphy.go
+
+Usage
+
+The command line client consists of a few sub commands.
+
+		Commands:
+			search, s           [args]
+			gif, id             [args]
+			random, rand, r     [args]
+			translate, trans, t [args]
+			trending, trend, tr [args]
+
+*/
 package main
 
 import (
