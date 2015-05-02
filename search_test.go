9b736e898f15c11f97e3039380f13b3b605815bb
Peter Hellberg <peter@c7.se>
2015-05-02 10:13:52 +0200

    Table based test for search

--- search_test.go
+++ search_test.go
@@ -6,35 +6,56 @@ import (
 )
 
 func TestSearch(t *testing.T) {
-	requests := []*http.Request{}
-	server, client := jsonServerAndClient(200, `{
-		"meta": {
-			"status": 200,
-			"msg": "OK"
+	for _, tt := range []struct {
+		q string
+	}{
+		{"foo"},
+		{"bar"},
+	} {
+		requests := []*http.Request{}
+		server, client := jsonServerAndClient(200, `{
+			"meta": {
+				"status": 200,
+				"msg": "OK"
+			}
+		}`, &requests)
+		defer server.Close()
+
+		search, err := client.Search([]string{tt.q})
+		if err != nil {
+			t.Errorf(`unexpected error %v`, err)
 		}
-	}`, &requests)
-	defer server.Close()
 
-	search, err := client.Search([]string{"foo"})
-	if err != nil {
-		t.Errorf(`unexpected error %v`, err)
-	}
+		if got, want := search.Meta.Status, 200; got != want {
+			t.Errorf(`search.Meta.Status = %+v, want %+v`, got, want)
+		}
 
-	if got, want := search.Meta.Status, 200; got != want {
-		t.Errorf(`search.Meta.Status = %+v, want %+v`, got, want)
-	}
+		if got := len(requests); got != 1 {
+			t.Fatalf(`unexpected number of requests %d`, got)
+		}
 
-	if got := len(requests); got != 1 {
-		t.Fatalf(`unexpected number of requests %d`, got)
-	}
+		r := requests[0]
 
-	r := requests[0]
+		if got := r.URL.Path; got != "/v1/gifs/search" {
+			t.Errorf(`unexpected path %#v`, got)
+		}
 
-	if got := r.URL.Path; got != "/v1/gifs/search" {
-		t.Errorf(`unexpected path %#v`, got)
-	}
+		params := r.URL.Query()
 
-	if got := r.URL.RawQuery; got != "api_key=dc6zaTOxFJmzC&limit=10&q=foo&rating=g" {
-		t.Errorf(`unexpected query %#v`, got)
+		if got := params.Get("api_key"); got != "dc6zaTOxFJmzC" {
+			t.Errorf(`unexpected api_key %#v`, got)
+		}
+
+		if got := params.Get("limit"); got != "10" {
+			t.Errorf(`unexpected limit %#v`, got)
+		}
+
+		if got := params.Get("rating"); got != "g" {
+			t.Errorf(`unexpected rating %#v`, got)
+		}
+
+		if got := params.Get("q"); got != tt.q {
+			t.Errorf(`unexpected q %#v`, got)
+		}
 	}
 }
